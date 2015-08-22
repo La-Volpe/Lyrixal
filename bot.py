967ce551c21a087089532bedfa36820683580d46
Javad Arjmandi <javadarjmandi@gmail.com>
2015-08-22 04:50:58 +0430

    Adds the /find command, fixes a few bugs, adds new easter egg

--- bot.py
+++ bot.py
@@ -7,13 +7,15 @@ API_TOKEN = token
 bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_message(message):
-    bot.reply_to(message, """Use /lyric [artist name]-[songe name]
-    Example: /lyric Slipknot-Snuff""")
+    bot.reply_to(message, """/lyric [artist name]-[songe name]
+    /find [Artist Name] - [Song Name]
+    Example:
+    /lyric Slipknot-Snuff""")
 
 @bot.message_handler(commands=['lyric'])
 def get_lyrics(message):
     raw = message.text
-    if len(raw) < 6 :
+    if len(raw) < 7 :
         bot.reply_to(message, "Oops! You didn't say anything! Try /help to learn more.")
     else:
         info = raw[5: len(raw)]
@@ -23,6 +25,19 @@ def get_lyrics(message):
         xml = utilities.fetch_lyrics(artist, song)
         lyric = utilities.parser(xml, "Lyric")
         bot.reply_to(message, lyric)
+@bot.message_handler(commands=['find'])
+def find(message):
+    raw = message.text
+    if len(raw) < 6 :
+        bot.reply_to(message, "Oops! You didn't say anything! Try /help to learn more.")
+    else:
+        info = raw[5: len(raw)]
+        info = info.split("-")
+        artist = info[0]
+        song = info[1]
+        _json = utilities.fetch_from_wiki(artist, song)
+        response = utilities.response(_json)
+        bot.reply_to(message, response)
 
 # Buncha Easter eggs below
 # They are starting to outgrow the main functionality!
@@ -30,35 +45,41 @@ MehdiSticker = 'BQADBAADDAIAAsY-4gABkYqH6Pg8d3UC'
 JavadSticker = 'BQADBAADLgQAApv7sgABhbe8T1HBWC4C'
 MohsenSticker = 'BQADBAADfwkAAhRfVQOHXHZgd1N9-gI'
 SinaSticker = 'BQADBAADhAQAApv7sgABfrYz202X0VUC'
+MahyarSticker = 'BQADBAADiwkAAhRfVQN3IC-BPzbIlQI'
 
 @bot.message_handler(commands=['easter', 'eggs', 'eastereggs'])
 def javad(message):
-    bot.reply_to(message, """/javad 
-    /mehdi or /facepalm 
-    /mohsen 
-    /sina""")
+    bot.reply_to(message, """/javad
+    /mehdi or /facepalm
+    /mohsen
+    /sina
+    /mahyar""")
 
-@bot.message_handler(commands=['javad'])
+
+@bot.message_handler(commands=['javad','Javad'])
 def javad(message):
     bot.send_sticker(message.chat.id, JavadSticker)
 
-@bot.message_handler(commands=['mehdi','facepalm'])
+@bot.message_handler(commands=['Mehdi','mehdi','facepalm'])
 def facepalm(message):
-    bot.send_sticker(message.chat.id, MehdiSticker)     
+    bot.send_sticker(message.chat.id, MehdiSticker)
 
-@bot.message_handler(commands=['mohsen'])
+@bot.message_handler(commands=['mohsen', 'Mohsen'])
 def keephydrated(message):
-    bot.send_sticker(message.chat.id, MohsenSticker)      
+    bot.send_sticker(message.chat.id, MohsenSticker)
 
-@bot.message_handler(commands=['sina'])
+@bot.message_handler(commands=['sina','Sina'])
 def keephydrated(message):
-    bot.send_sticker(message.chat.id, SinaSticker)   
+    bot.send_sticker(message.chat.id, SinaSticker)
 
+@bot.message_handler(commands=['Mahyar','mahyar'])
+def keephydrated(message):
+    bot.send_sticker(message.chat.id, MahyarSticker)
 #@bot.message_handler(func=lambda message: True, content_types=['sticker'])
 #def getid(message):
 #    fileid = message.sticker.file_id
 #    bot.reply_to(message, fileid)
-    
+
 
 bot.polling()
 
--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import urllib.request as ur
+import demjson as json
 #To build url
 def url_builder(artist, song):
     getVars = {'artist' : '', 'song':''}
@@ -14,13 +15,40 @@ def fetch_lyrics(artist, song):
     response = request.read()
     xml = response.decode("utf-8")
     return xml
-#Parses the xml and then returns the 
+#Parses the xml and then returns the
 def parser(xml, tag):
     tag = '<'+ tag + '>'
     index = xml.find(tag)
     #If the index is -1, it means that the <Lyric> tag is not found, So the lyric does not exist:
     if index == -1:
-        return "Song not found!"
+        return "Song not found!\nTry:\n/find [Artist Name] - [Song Name]\nfor a better result."
     else:
-        lyric = xml[index+7 : len(xml) - 27]
+        lyric = xml[index+7 : - 27]
         return lyric
+#---------------------------------------------------------------
+#builds the link to lyrics wikia...
+def wikia_url_builder(artist, song):
+    getVars = {'func':'getSong','artist' : '', 'song':'','fmt':'json'}
+    getVars['artist'] = artist
+    getVars['song'] = song
+    url = 'http://lyrics.wikia.com/api.php?'
+    url = url + urllib.parse.urlencode(getVars)
+    return url
+#Fetches the preview and the link to lyrics wikia...
+def fetch_from_wiki(artist, song):
+    url = wikia_url_builder(artist, song)
+    request = ur.urlopen(url).read().decode('utf-8')
+    request = request[7:len(request)]
+    _json = json.decode(request)
+    return _json
+#Prepares the proper response:
+def response(fetched):
+    lyric = fetched['lyrics']
+    if lyric == "Not found":
+        return "Lyrics not found!"
+    else:
+        url = fetched['url']
+        resp = lyric+"\n"+url
+        return resp
+#song = fetch('Brendan Perry', 'crescent')
+#resp = response(song)
